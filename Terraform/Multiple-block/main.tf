ed5113cc482e04209035781be6bbdd79abeb6498
Chaitalykundu <chaitaly.slg01@gmail.com>
2025-10-14 21:28:56 +0530

    terraform ne wcode

---
+++ Terraform/Multiple-block/main.tf
@@ -0,0 +1,11 @@
+output "firstOutput"{
+    value = "This is first output block"
+}
+
+output "secondOutput"{
+    value = "This is second output block"
+}
+
+output "thirdOutput"{
+    value = "This is third output block"
+}
