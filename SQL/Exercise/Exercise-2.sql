fdedf028b461a2dc3760f2390c2e0f62f59e691a
Bhavesh Mahajan <144446606+BhaveshMahajan006@users.noreply.github.com>
2025-10-25 11:39:32 +0530

    Create Exercise-2.sql

---
+++ SQL/Exercise/Exercise-2.sql
@@ -0,0 +1,10 @@
+
+-- 2. Given the `books` table, write a SQL query to:
+--    - Select all columns from the `books` table.
+--    - Order the results by the `price` in descending order.
+SELECT
+    *
+FROM
+    books
+order by
+    price desc;
