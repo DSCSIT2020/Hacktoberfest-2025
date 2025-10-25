ecb050a6689d2b06e00621c62920ce42ad587f26
UTSAV OJHA <ajatsatru787@gmail.com>
2025-10-26 01:11:38 +0530

    Create binary-search.py

---
+++ python/binary-search.py
@@ -0,0 +1,13 @@
+def binary_search_recursive(arr, target, left, right):
+    if left > right:
+        return -1
+    
+    mid = (left + right) // 2
+    
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search_recursive(arr, target, mid + 1, right)
+    else:
+        return binary_search_recursive(arr, target, left, mid - 1)
+
